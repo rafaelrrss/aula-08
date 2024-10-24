8f376f3b8ae38a55bb0247f1a0bd86427d56988b
rafaelrrss <116085595+rafaelrrss@users.noreply.github.com>
2024-10-24 16:05:59 +0000

    php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 8</title>
+</head>
+<body>
+    <h1>Introdução à programação backend</h1>
+    <?php
+    echo 'olá, Rafael';
+
+    $ano_nascimento = 1983;
+    $ano_atual = 2024;
+    $idade = $ano_atual - $ano_nascimento;
+
+    echo 'você possui '. $idade . ' anos'
+    ?>
+
+
+    
+</body>
+</html>
